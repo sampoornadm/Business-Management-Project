a79fed7ba03655395d806081782d66357be74a76
Sombuddha Chatterjee <chatterjeesombuddha@gmail.com>
2026-07-12 18:40:07 +0530

    feat(ui): add PageHeader component

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -25,6 +25,7 @@ export * from "./components/dropdown-menu";
 export * from "./components/multi-select";
 export * from "./components/skeleton";
 export * from "./components/pagination";
+export * from "./components/page-header";
 export * from "./components/separator";
 export * from "./components/stepper";
 export * from "./components/tabs";
